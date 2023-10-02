93eeb843a256bb71e95ce1a17e2a5ae00dac5e30
LauriRodrigues <laurianarodrigueslima@gmail.com>
2023-10-02 11:44:20 -0300

    History page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,7 +1,64 @@
+import { HistoryContainer, HistoryList, Status } from './styles'
+
 export function History() {
   return (
-    <>
-      <h1>History</h1>
-    </>
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa X</td>
+              <td>25 minutos</td>
+              <td>Há dois meses</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa X</td>
+              <td>25 minutos</td>
+              <td>Há dois meses</td>
+              <td>
+                <Status statusColor="red">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa X</td>
+              <td>25 minutos</td>
+              <td>Há dois meses</td>
+              <td>
+                <Status statusColor="yellow">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa X</td>
+              <td>25 minutos</td>
+              <td>Há dois meses</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa X</td>
+              <td>25 minutos</td>
+              <td>Há dois meses</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
   )
 }
